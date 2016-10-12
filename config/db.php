80c9922ca68446d8d1bfd29b198439dd248b44f0
kevin daus <kevinflorenzdaus@gmail.com>
2016-10-12 22:29:56 +0800

    changed database confuigration to fit current env

--- config/db.php
+++ config/db.php
@@ -1,9 +1,19 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=reportsma_esign',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
+$dbConfiguration = [
+	    'class' => 'yii\db\Connection',
+	    'dsn' => 'mysql:host=localhost;dbname=site8_esign',
+	    'username' => 'site8_esign',
+	    'password' => 'hitman052529',
+	    'charset' => 'utf8',
+	];
+if (defined('YII_DEBUG') && defined('YII_ENV') == 'dev') {
+	$dbConfiguration =  [
+	    'class' => 'yii\db\Connection',
+	    'dsn' => 'mysql:host=localhost;dbname=reportsma_esign',
+	    'username' => 'root',
+	    'password' => '',
+	    'charset' => 'utf8',
+	];
+}
+return $dbConfiguration;
