c7f8c500629c1371ce9494fea65f8b9ceba76033
Your Name <you@example.com>
2016-12-07 18:25:48 +0800

    dsa

--- controllers/SignatureController.php
+++ controllers/SignatureController.php
@@ -23,7 +23,7 @@ class SignatureController extends \yii\web\Controller
                 if (isset($requestedLead->declaration_confirmed_tick) && is_array($requestedLead->declaration_confirmed_tick)) {
                     $requestedLead->declaration_confirmed_tick = implode(",", $requestedLead->declaration_confirmed_tick);
                 }
-                // $requestedLead->after_upgrade_already_has_products = $_POST['LeadEsign']['after_upgrade_already_has_products'];
+
                 $requestedLead->on(LeadEsign::SIGNATURE_FINAL_STEP, ['app\models\events\ClientSignatureLead', 'handle'],$requestedLead);
                 $requestedLead->saveClientSignature();
                 if ($requestedLead->save()) {
@@ -43,32 +43,32 @@ class SignatureController extends \yii\web\Controller
             throw new NotFoundHttpException("Record doesn't exists.");
         }
     }
-    public function actionTest($securityKey)
-    {
-        /**
-         * @var $requestedLead LeadEsign
-         */
-        $requestedLead = LeadEsign::find()->where(['security_key' => $securityKey])->one();
-        if ($requestedLead) {
-            //load and save
-            if ($requestedLead->load(\Yii::$app->request->post())) {
-                // $requestedLead->after_upgrade_already_has_products = $_POST['LeadEsign']['after_upgrade_already_has_products'];
-                $requestedLead->on(LeadEsign::SIGNATURE_FINAL_STEP, ['app\models\events\ClientSignatureLead', 'handle'],$requestedLead);
-                $requestedLead->saveClientSignature();
-                if ($requestedLead->save()) {
-                    \Yii::$app->session->setFlash('success', "Success!");
-                    $requestedLead->trigger(LeadEsign::SIGNATURE_FINAL_STEP);
-                    return $this->redirect("/success");
-                }
-            }
-            $requestedLead->account_start_date = date("d-m-Y", strtotime($requestedLead->account_start_date));
-            $requestedLead->account_end_date = date("d-m-Y", strtotime($requestedLead->account_end_date));
-            $requestedLead->date_of_birth = date("d-m-Y", strtotime($requestedLead->date_of_birth));
-            $requestedLead->after_upgrade_already_has_products = explode(",", $requestedLead->after_upgrade_already_has_products);
-            return $this->render('test', ['model' => $requestedLead]);
-        } else {
-            throw new NotFoundHttpException("Record doesn't exists.");
-        }
-    }
+    // public function actionTest($securityKey)
+    // {
+    //     /**
+    //      * @var $requestedLead LeadEsign
+    //      */
+    //     $requestedLead = LeadEsign::find()->where(['security_key' => $securityKey])->one();
+    //     if ($requestedLead) {
+    //         //load and save
+    //         if ($requestedLead->load(\Yii::$app->request->post())) {
+    //             // $requestedLead->after_upgrade_already_has_products = $_POST['LeadEsign']['after_upgrade_already_has_products'];
+    //             $requestedLead->on(LeadEsign::SIGNATURE_FINAL_STEP, ['app\models\events\ClientSignatureLead', 'handle'],$requestedLead);
+    //             $requestedLead->saveClientSignature();
+    //             if ($requestedLead->save()) {
+    //                 \Yii::$app->session->setFlash('success', "Success!");
+    //                 $requestedLead->trigger(LeadEsign::SIGNATURE_FINAL_STEP);
+    //                 return $this->redirect("/success");
+    //             }
+    //         }
+    //         $requestedLead->account_start_date = date("d-m-Y", strtotime($requestedLead->account_start_date));
+    //         $requestedLead->account_end_date = date("d-m-Y", strtotime($requestedLead->account_end_date));
+    //         $requestedLead->date_of_birth = date("d-m-Y", strtotime($requestedLead->date_of_birth));
+    //         $requestedLead->after_upgrade_already_has_products = explode(",", $requestedLead->after_upgrade_already_has_products);
+    //         return $this->render('test', ['model' => $requestedLead]);
+    //     } else {
+    //         throw new NotFoundHttpException("Record doesn't exists.");
+    //     }
+    // }
 
 }
