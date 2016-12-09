775e5c550f7b1076304952f0b85ba54b13bfdd73
Your Name <you@example.com>
2016-12-09 22:32:41 +0800

    changed email message for new leads

--- models/events/NewLeadEventHandler.php
+++ models/events/NewLeadEventHandler.php
@@ -23,14 +23,31 @@ class NewLeadEventHandler extends Event{
          */
         $currentLeadData = $event->data;
         $signaturelinkUrl ="<a href='".Url::home(true).'signature/'.$currentLeadData->security_key."'> Final step </a>";
-         
+        $logoPath = \Yii::getAlias("@app/web/images/moneyactive.JPG");
+        $type = pathinfo($logoPath, PATHINFO_EXTENSION);
+        $imgLogo = file_get_contents($logoPath);
+        $base64Img = 'data:image/' . $type . ';base64,' . base64_encode($imgLogo);
+        $templateMessage =  <<<EOL
+Hi $customerName,
+<br>
+<br>
+As discussed, please find attached esign documents ready for signature.
+<br>
+<br>
+Please complete this as soon as possible to start your claim. $signaturelinkUrl
+<br>
+<br>
+Kind regards<br>
+Money Active<br>
+<img src='$base64Img' />
+EOL;         
         /*send the email*/           
         \Yii::$app->mailer->compose()
             ->setFrom('esign@site8.co')
             ->setTo($currentLeadData->email_address)
             // ->setTo("hellsing357@gmail.com")
             ->setSubject('Final Step : Signature')
-            ->setHtmlBody("Please finalize your claim $signaturelinkUrl.")
+            ->setHtmlBody($templateMessage)
             ->send();      
     }
 } 
